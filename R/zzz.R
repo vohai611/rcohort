e241529b111d9e9978025193ecad3621dd730af9
Haivo <hai835559@gmail.com>
2022-04-05 14:50:12 +0700

    fix test to pass check()

--- R/zzz.R
+++ R/zzz.R
@@ -1,1 +1,1 @@
-globalVariables(c("variable", "value", "time_col2", ".", "pct", "n", "relative_period", "variable", "value", ".N"))
+globalVariables(c("variable", "value", "time_col2", ".", "pct", "n", "relative_period", "variable", "value", ".N", "cohort"))
